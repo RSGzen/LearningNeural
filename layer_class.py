95ce30173e1f72812c426c38301f5cfff46661f5
Ge Rui Sen <greyson.rg@gmail.com>
2024-09-13 23:01:24 +0800

    Added forward pass and separated class script

---
+++ layer_class.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Layer_Dense:
+    def __init__(self, num_inputs, num_neurons):
+        self.num_inputs = num_inputs
+        self.num_neurons = num_neurons
+        self.weights = np.random.rand(num_inputs, num_neurons)
+        self.biases = np.zeros((1, num_neurons))
+        self.output = None
+
+    def forward_pass(self, input):
+        self.output = input @ self.weights + self.biases
+
+    def backpropagation(self):
+        pass
