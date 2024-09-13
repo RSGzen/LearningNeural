97d6202f23d2f7d00a647c8d81caea778d08a7bb
Ge Rui Sen <greyson.rg@gmail.com>
2024-09-13 13:24:10 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+inputs = [[1.0, 2.0, 3.0, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backpropagation(self):
+        pass
