dc9f571eac168702b120233b43963155820fde2b
Ge Rui Sen <greyson.rg@gmail.com>
2024-09-13 22:44:36 +0800

    Added Layer Class

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ inputs = [[1.0, 2.0, 3.0, 2.5],
           [-1.5, 2.7, 3.3, -0.8]]
 
 class Layer_Dense:
-    def __init__(self):
-        pass
+    def __init__(self, num_inputs, num_neurons):
+        self.num_inputs = num_inputs
+        self.num_neurons = num_neurons
+        self.weights = np.random.rand(num_inputs, num_neurons)
+        self.biases = np.zeros((1, num_inputs))
+        self.output = None
 
     def forward(self):
         pass
