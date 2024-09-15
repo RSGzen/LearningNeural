bea9c4f17e48f79fda4e1acfb3df97ea8f331ee4
Ge Rui Sen <greyson.rg@gmail.com>
2024-09-15 23:37:27 +0800

    Added clear output methods

--- activation_func_class.py
+++ activation_func_class.py
@@ -4,6 +4,9 @@ class Activation_Function:
     def __init__(self):
         self.output = np.empty()
     
+    def clear_output(self):
+        self.output = np.empty()
+    
     def relu(self, input):
         for i in input:
             self.output.append(max(0, i))
@@ -19,4 +22,4 @@ class Activation_Function:
             exp_positive = np.exp(i)
             exp_negative = np.exp(-i)
             result = (exp_positive - exp_negative) / (exp_positive + exp_negative)
-            self.output.append(result) 
+            self.output.append(result)
